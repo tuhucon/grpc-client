499ee36ee8173f8b9e87ad0a8189e16b28b9c919
LAP01171 <lap01171@LAP01171s-MacBook-Pro.local>
2019-06-14 12:14:04 +0700

    server stream sync

--- src/main/java/com/example/grpc/client/ClientApplication.java
+++ src/main/java/com/example/grpc/client/ClientApplication.java
@@ -2,12 +2,17 @@ package com.example.grpc.client;
 
 import com.example.grpc.server.message.HelloRequest;
 import com.example.grpc.server.message.HelloResponse;
+import com.example.grpc.server.message.ServerStreamRequest;
+import com.example.grpc.server.message.ServerStreamResponse;
 import com.example.grpc.server.service.HelloServiceGrpc;
 
+import com.example.grpc.server.service.StreamServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.function.Consumer;
 
 
 @SpringBootApplication
@@ -17,10 +22,17 @@ public class ClientApplication {
         SpringApplication.run(ClientApplication.class, args);
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext().build();
-        HelloServiceGrpc.HelloServiceBlockingStub helloService = HelloServiceGrpc.newBlockingStub(channel);
+//        HelloServiceGrpc.HelloServiceBlockingStub helloService = HelloServiceGrpc.newBlockingStub(channel);
 
-        HelloResponse response = helloService.hello(HelloRequest.newBuilder().setFirstName("minh").setLastName("tuan").build());
-        System.out.println(response);
+//        HelloResponse response = helloService.hello(HelloRequest.newBuilder().setFirstName("minh").setLastName("tuan").build());
+//        System.out.println(response);
+
+
+        StreamServiceGrpc.StreamServiceBlockingStub streamService = StreamServiceGrpc.newBlockingStub(channel);
+        streamService.serverStream(ServerStreamRequest.newBuilder().setStart(10).build()).forEachRemaining(
+                serverStreamResponse -> System.out.println(serverStreamResponse.getNext())
+        );
+
         Thread.sleep(30_000);
         channel.shutdown();
 
