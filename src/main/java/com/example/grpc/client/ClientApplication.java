3a777557917cc9951d679d90a32d945afcaa466a
LAP01171 <lap01171@LAP01171s-MacBook-Pro.local>
2019-06-17 14:16:26 +0700

    server intercep, just intercep first time not for all message in a stream

--- src/main/java/com/example/grpc/client/ClientApplication.java
+++ src/main/java/com/example/grpc/client/ClientApplication.java
@@ -35,73 +35,73 @@ public class ClientApplication {
 //        );
 
 
-        //Client Streaming
-//        StreamServiceGrpc.StreamServiceStub streamService = StreamServiceGrpc.newStub(channel);
-//        StreamObserver<ClientStreamRequest> request = streamService.clientStream(new StreamObserver<ClientStreamResponse>() {
-//            @Override
-//            public void onNext(ClientStreamResponse value) {
-//                System.out.println(value.getCount());
-//            }
-//
-//            @Override
-//            public void onError(Throwable t) {
-//
-//            }
-//
-//            @Override
-//            public void onCompleted() {
-//
-//            }
-//        });
-//        for (int i = 0; i < 10; i++) {
-//            ClientStreamRequest value = ClientStreamRequest.newBuilder()
-//                    .setNext(i)
-//                    .build();
-//            request.onNext(value);
-//            Thread.sleep(1_000L);
-//        }
-//        request.onCompleted();
-
-        //Bidirection Streaming
-        AtomicReference<StreamObserver<BidirectionStreamRequest>> ref = new AtomicReference<>();
+//        Client Streaming
         StreamServiceGrpc.StreamServiceStub streamService = StreamServiceGrpc.newStub(channel);
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        StreamObserver<BidirectionStreamRequest> request = streamService.bidirectionStream(new StreamObserver<BidirectionStreamResponse>() {
-            AtomicInteger count = new AtomicInteger(0);
-
+        StreamObserver<ClientStreamRequest> request = streamService.clientStream(new StreamObserver<ClientStreamResponse>() {
             @Override
-            public void onNext(BidirectionStreamResponse value) {
-                System.out.println(value.getMsg());
-                if (count.addAndGet(1) < 100) {
-                    System.out.println("count = " + count + " client ping");
-                    BidirectionStreamRequest x = BidirectionStreamRequest.newBuilder()
-                            .setMsg("client ping")
-                            .build();
-                    ref.get().onNext(x);
-                } else {
-                    countDownLatch.countDown();
-                }
+            public void onNext(ClientStreamResponse value) {
+                System.out.println(value.getCount());
             }
 
             @Override
             public void onError(Throwable t) {
-                countDownLatch.countDown();
-                System.out.println(t.getMessage());
+
             }
 
             @Override
             public void onCompleted() {
-                countDownLatch.countDown();
-                System.out.println("server is completed");
+
             }
         });
-        ref.set(request);
-        System.out.println("count = 0 client ping");
-        request.onNext(BidirectionStreamRequest.newBuilder()
-                .setMsg("client ping")
-                .build());
-        countDownLatch.await();
+        for (int i = 0; i < 10; i++) {
+            ClientStreamRequest value = ClientStreamRequest.newBuilder()
+                    .setNext(i)
+                    .build();
+            request.onNext(value);
+            Thread.sleep(1_000L);
+        }
         request.onCompleted();
+
+        //Bidirection Streaming
+//        AtomicReference<StreamObserver<BidirectionStreamRequest>> ref = new AtomicReference<>();
+//        StreamServiceGrpc.StreamServiceStub streamService = StreamServiceGrpc.newStub(channel);
+//        CountDownLatch countDownLatch = new CountDownLatch(1);
+//        StreamObserver<BidirectionStreamRequest> request = streamService.bidirectionStream(new StreamObserver<BidirectionStreamResponse>() {
+//            AtomicInteger count = new AtomicInteger(0);
+//
+//            @Override
+//            public void onNext(BidirectionStreamResponse value) {
+//                System.out.println(value.getMsg());
+//                if (count.addAndGet(1) < 100) {
+//                    System.out.println("count = " + count + " client ping");
+//                    BidirectionStreamRequest x = BidirectionStreamRequest.newBuilder()
+//                            .setMsg("client ping")
+//                            .build();
+//                    ref.get().onNext(x);
+//                } else {
+//                    countDownLatch.countDown();
+//                }
+//            }
+//
+//            @Override
+//            public void onError(Throwable t) {
+//                countDownLatch.countDown();
+//                System.out.println(t.getMessage());
+//            }
+//
+//            @Override
+//            public void onCompleted() {
+//                countDownLatch.countDown();
+//                System.out.println("server is completed");
+//            }
+//        });
+//        ref.set(request);
+//        System.out.println("count = 0 client ping");
+//        request.onNext(BidirectionStreamRequest.newBuilder()
+//                .setMsg("client ping")
+//                .build());
+//        countDownLatch.await();
+//        request.onCompleted();
 
         Thread.sleep(1_000L);
         channel.shutdown();
